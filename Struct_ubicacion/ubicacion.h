ab72c7a5855f0b903bf0a2ef7ee32a96eb10dfbb
pabloccf <palogo2001@hotmail.es>
2020-12-17 13:59:56 +0100

    struct_ubicacion

---
+++ Struct_ubicacion/ubicacion.h
@@ -0,0 +1,12 @@
+#ifndef UBICACION_H
+#define UBICACION_H
+
+#include <string>
+
+struct ubicacion{
+    std::string localizacion;
+    std::string municipio;
+    std::string provincia;
+};
+
+#endif
