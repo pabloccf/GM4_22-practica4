f70fd0da3567f49e16e2e1480b67ddaec9888010
pabloccf <palogo2001@hotmail.es>
2020-12-19 15:04:01 +0100

    struct_date

---
+++ Struct_fecha/date.h
@@ -0,0 +1,21 @@
+#ifndef DATE_H
+#define DATE_H
+
+#include <iostream>
+
+struct date{
+    int dia;
+    int mes;
+    int year;
+
+    bool operator==(const date f)const{
+        return (dia==f.dia && mes==f.mes && year==f.year);
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const date &f){
+        os<<f.dia<<"/"<<f.mes<<"/"<<f.year;
+        return os;
+    }
+};
+
+#endif
